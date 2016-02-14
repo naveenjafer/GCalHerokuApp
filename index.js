158453c6aedd464e82ff5bc2160dada0273ac684
Naveen13 <naveenjafer@outlook.com>
2016-02-14 22:49:59 +0530

    Simple server functionality added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,6 +17,7 @@ app.get('/page1', function(request, response) {
 });
 
 app.post('/page2',function(request, response) {
+  response.send(request.body);
 response.render('pages/index');
 })
 
