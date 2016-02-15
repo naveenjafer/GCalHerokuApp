ce19964a003355a13737a39eefa5cb4597164b83
Naveen13 <naveenjafer@outlook.com>
2016-02-15 10:50:23 +0530

    make it better

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+app.use(cors());
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
  });
 
