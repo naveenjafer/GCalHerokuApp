495f02fd430d642d77595ff65e7590a187b705f3
Naveen13 <naveenjafer@outlook.com>
2016-02-15 10:08:18 +0530

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
